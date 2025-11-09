edf11d8ca405e93a4e367c81848d87c4d4054232
Eric Curtin <eric.curtin@docker.com>
2025-11-09 18:57:50 +0000

    Add progress bar
    And remove existing transport implementation
    
    We are gonna implement this in the go-containerregistry layer it's cleaner
    
    Signed-off-by: Eric Curtin <eric.curtin@docker.com>

--- cmd/cli/commands/push.go
+++ cmd/cli/commands/push.go
@@ -35,7 +35,8 @@ func newPushCmd() *cobra.Command {
 }
 
 func pushModel(cmd *cobra.Command, desktopClient *desktop.Client, model string) error {
-	response, progressShown, err := desktopClient.Push(model, TUIProgress)
+	printer := asPrinter(cmd)
+	response, progressShown, err := desktopClient.Push(model, printer)
 
 	// Add a newline before any output (success or error) if progress was shown.
 	if progressShown {
